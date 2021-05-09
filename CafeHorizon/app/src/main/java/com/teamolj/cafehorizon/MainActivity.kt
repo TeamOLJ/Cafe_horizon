87fb0f13afda5aadd87e6a4bdb54241db1b413cf
Hyeon_ <onhyeon618@gmail.com>
2021-05-09 19:14:31 +0900

    Primary settings before the work begin

--- CafeHorizon/app/src/main/java/com/teamolj/cafehorizon/MainActivity.kt
+++ CafeHorizon/app/src/main/java/com/teamolj/cafehorizon/MainActivity.kt
@@ -2,10 +2,14 @@ package com.teamolj.cafehorizon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.teamolj.cafehorizon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
