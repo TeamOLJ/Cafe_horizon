ba59cb102163e7305159958f65e561650a35e088
hyeon_ <onhyeon618@gmail.com>
2021-08-26 21:59:12 +0900

    Minor edits after merge

---
+++ CafeHorizon/app/src/main/java/com/teamolj/cafehorizon/smartOrder/AppDatabase.kt
@@ -0,0 +1,25 @@
+package com.teamolj.cafehorizon.smartOrder
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Cart::class], version = 3)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun cartDao(): CartDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context, AppDatabase::class.java, "orm_cart")
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build()
+            }
+            return INSTANCE as AppDatabase
+        }
+    }
+}
